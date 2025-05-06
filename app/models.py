9caa9594cd1d5945f67a4df87e39bfe1c2f618d5
Dmitriy <pereguda.da@medsigroup.ru>
2025-05-06 15:43:46 +0300

    uuid -> int books

--- app/models.py
+++ app/models.py
@@ -1,13 +1,11 @@
 from datetime import datetime
-import uuid
 from sqlalchemy import Column, String, Float, Integer, DateTime
-from sqlalchemy.dialects.postgresql import UUID
 from db.base_class import Base
 
 class Book(Base):
     __tablename__ = "books"
 
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     title = Column(String, nullable=False)
     author = Column(String, nullable=False)
     price = Column(Float, nullable=False)
